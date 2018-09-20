393f8c53653f0cab11917f14588711f92c9a77ca
Gianca210694 <giancarlo.manzano94@gmail.com>
2018-09-20 17:45:01 -0500

    test-heroku
    nuevo-heroku

--- src/main/java/org/transport420/sgmv/dao/interfaces/IUsuarioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package org.transport420.sgmv.dao.interfaces;
-
-import org.transport420.sgmv.model.Usuario;
-
-public interface IUsuarioRepositorio {
-
-	public abstract Usuario login(Usuario usuario) throws Exception;
-	
-}
